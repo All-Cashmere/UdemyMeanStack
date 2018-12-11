4a8c37a4bb648207ac686ff217d74db09b7990b4
Dakyung Lee <101079446@georgebrown.ca>
2018-12-11 17:47:53 -0500

    backend with delete

--- frontend/backend/app.js
+++ frontend/backend/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const Post = require("./models/post");
 const app = express();
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0-mqa2b.mongodb.net/test?retryWrites=true")
+.then(()=> {
+    console.log("Yay database!")
+})
+.catch(() => {
+    console.log("Uh oh :(")
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,29 +33,29 @@ app.post("/api/posts", (req, res, next) => {
       title: req.body.title,
       content: req.body.content
   });
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  post.save().then(createdPost=>{
+    res.status(201).json({
+    message: "Post added successfully",
+    postId: createdPost._id
+    });
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
+  Post.find()
+  .then(documents => {
   res.status(200).json({
     message: "Posts fetched successfully!",
-    posts: posts
+    posts: documents
   });
+});
+});
+
+app.delete("/api/posts/:id", (req, res, next)=>{
+    Post.deleteOne({_id: req.params.id}).then(result=>{
+        console.log(result);
+        res.status(200).json({message: "Post deleted"});
+    });
 });
 
 module.exports = app;
--- frontend/src/app/posts/post-create/post-create.component.ts
+++ frontend/src/app/posts/post-create/post-create.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
-import {NgForm} from '@angular/forms';
-import { PostsService } from '../posts.service';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { PostsService } from "../posts.service";
+
 @Component({
-  selector: 'app-post-create',
-  templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create.component.css"]
 })
 export class PostCreateComponent {
-  enteredTitle = '';
-  enteredContent = '';
-
+  enteredTitle = "";
+  enteredContent = "";
 
   constructor(public postsService: PostsService) {}
+
   onAddPost(form: NgForm) {
     if (form.invalid) {
       return;
